9bff6dbde02d3497df47d8aed1e1be7e5da1c461
Oscar Martinez <145166121+Oscaretz@users.noreply.github.com>
2024-04-18 15:44:09 -0600

    Update google_scraper.py

--- google_scraper.py
+++ google_scraper.py
@@ -4,8 +4,8 @@ from fake_useragent import UserAgent
 
 
 class GoogleSearchNews:
-    def __init__(self, num_pages, query, qexception, qoption, qrangedate, qsite):
-        self.num_pages = num_pages
+    def __init__(self, nqueries, query, qexception, qoption, qrangedate, qsite, ):
+        self.nqueries = nqueries
         self.noticias = {}
         self.user_agent = UserAgent()
         self.query = query
@@ -13,6 +13,7 @@ class GoogleSearchNews:
         self.qexception = qexception
         self.qrangedate = qrangedate
         self.qsite = qsite
+
 
     async def get_info_new(self, session, url):
         async with session.get(url, headers={'User-Agent': self.user_agent.random}) as response:
@@ -37,8 +38,8 @@ class GoogleSearchNews:
     async def get_all_news(self):
         tasks = []
         async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=10)) as session:
-            for page_num in range(1, self.num_pages + 1):
-                url = f"https://www.google.com/search?q={self.query.replace(',','%20')}+{self.qoption.replace(',','+OR')}+-{self.qexception.replace(',','+-')}+site%3A{self.qsite}&lr=&sca_esv=2cdbf9d7674441ef&sxsrf=ACQVn09LTw8Oz-QMTiJFOR7cBmlbGmXFjw%3A1711408457466&source=lnt&tbs=cdr%3A1%2Ccd_min%3A{self.qrangedate.replace('-','%2F').replace('/','%2Ccd_max%3A')}%2Cctr%3AcountryMX&tbm=nws&sxsrf=ACQVn08n6MZ5dLUWMKiVtyFXkWoTHss1PA%3A1709841442399&ei=IhzqZaz_F6arur8Pxr-4iAU&ved=0ahUKEwismK_e9-KEAxWmle4BHcYfDlEQ4dUDCA4&uact=5&oq=&gs_lp=Egxnd3Mtd2l6LW5ld3MiC2ludW5kYWNpw7NuMggQABiABBixAzIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEjKJFC8A1ioE3AAeACQAQCYAcUBoAGTEKoBBDEuMTa4AQPIAQD4AQGYAgagArIFwgIFECEYoAHCAgYQABgWGB6YAwCIBgGSBwMwLjagB7Mt&sclient=gws-wiz-news&start={page_num*10 -10}"
+            for page_num in range(1, int(self.nqueries) + 1):
+                url = f"https://www.google.com/search?q={self.query.replace(',','%20')}+{self.qoption.replace(',','+OR')}+-{self.qexception.replace(',','+-')}+site%3A{self.qsite}&lr=&sca_esv=2cdbf9d7674441ef&sxsrf=ACQVn09LTw8Oz-QMTiJFOR7cBmlbGmXFjw%3A1711408457466&source=lnt&tbs=cdr%3A1%2Ccd_min%3A{self.qrangedate.replace('-','%2F').replace('/','%2Ccd_max%3A')}%2Cctr%3AcountryMX&tbm=nws&sxsrf=ACQVn08n6MZ5dLUWMKiVtyFXkWoTHss1PA%3A1709841442399&ei=IhzqZaz_F6arur8Pxr-4iAU&ved=0ahUKEwismK_e9-KEAxWmle4BHcYfDlEQ4dUDCA4&uact=5&oq=&gs_lp=Egxnd3Mtd2l6LW5ld3MiC2ludW5kYWNpw7NuMggQABiABBixAzIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABEjKJFC8A1ioE3AAeACQAQCYAcUBoAGTEKoBBDEuMTa4AQPIAQD4AQGYAgagArIFwgIFECEYoAHCAgYQABgWGB6YAwCIBgGSBwMwLjagB7Mt&sclient=gws-wiz-news&start={page_num -10}"
                 tasks.append(self.get_info_new(session, url))
 
             await asyncio.gather(*tasks)
@@ -48,9 +49,9 @@ class GoogleSearchNews:
             json.dump(self.noticias, json_file, indent=4, ensure_ascii=False)
         return os.path.join('archivos', 'links.json')
 
-async def main(num_pages, query, qexception, qoption, qrangedate, qsite):
+async def main(nqueries, query, qexception, qoption, qrangedate, qsite):
 
-    scraper = GoogleSearchNews(num_pages, query, qexception, qoption, qrangedate, qsite)
+    scraper = GoogleSearchNews(nqueries, query, qexception, qoption, qrangedate, qsite)
     await scraper.get_all_news()
     result = scraper.save_to_json()
     return result
